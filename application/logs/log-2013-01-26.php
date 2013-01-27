10013b603480badb3e815b8ed165feb4c5a062cf
Drew B. Dennis <drewbdennis@gmail.com>
2013-01-27 19:12:52 +0800

    Delection
    1. Deleted protected folder to improve system design

---
+++ application/logs/log-2013-01-26.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-01-26 14:14:52 --> Severity: Warning  --> mysql_pconnect(): MySQL server has gone away C:\xampp\htdocs\elibrary\system\database\drivers\mysql\mysql_driver.php 91
